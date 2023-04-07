729e0e34f6cd29afb172dbeb823d6c1e8dc5ac53
cristobal <cristobal@jotaa.cl>
2023-04-07 15:27:10 -0400

    header listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import Header from './components/Header';
+
 import './sass/style.scss';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { getTheme } from './features/theme/themeSlice';
+
+const KEY_THEME = "monthlyexpenses.theme";
+
 function App() {
+
+  const dispatch = useDispatch();
+  const selectorTheme = useSelector(state => state.theme);
+
+  const body = document.querySelector('#body');
+
+  // Obtener la información del estado que se procesó previamente
+  useEffect(() => {
+    const storedTheme = JSON.parse(localStorage.getItem(KEY_THEME));
+
+    if(storedTheme){
+      dispatch(getTheme(storedTheme));
+    }
+
+  }, [dispatch]);
+
+  // Cada vez que el estado tenga cambios
+  useEffect(() => {
+
+    localStorage.setItem(KEY_THEME, JSON.stringify(selectorTheme));
+
+  }, [selectorTheme]);
+  
+
+  body.classList.contains('Light') && body.classList.remove('Light');
+  body.classList.contains('Dark') && body.classList.remove('Dark');
+
+  body.classList.add(selectorTheme.mode);
+
   return (
     <>
-     <h1 className='prueba'>Hola desde SASS</h1>
+      <Header selectorTheme={selectorTheme} />
     </>
   );
 }
