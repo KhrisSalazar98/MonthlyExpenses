5c0eca3d52ce2e1eb104bfee58f37ef03ba79b61
cristobal <cristobal@jotaa.cl>
2023-05-05 12:53:28 -0400

    comienza la refactorización del listado de gastos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faListOl, faPlus, faCalendarDays, faBookOpen, faHandHoldingDollar, faPenToSquare, faTrashCan, faCircleExclamation} from '@fortawesome/free-solid-svg-icons';
 
